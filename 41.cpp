fd0e81ee8f132cfead60c90020d6e59e80bc4485
RAJEEV KUAMR <96237505+70049@users.noreply.github.com>
2022-08-08 12:16:11 +0530

    Update 41.cpp

--- 41.cpp
+++ 41.cpp
@@ -3,26 +3,26 @@
 #include<conio.h>
 #include<fstream>
 using namespace std;
-void copyfile(char* f1,char* f2);
-void copyfile(char* f1,char* f2)
+void copyfile(char* f1,char* f2);      //FUNCTION DECLARATION//
+void copyfile(char* f1,char* f2)         //CREATE A NEW FUNCTION(copyfile)//
 {
-    ifstream fin;
-    ofstream fout;
+    ifstream fin;                    //ifstream is Function It'll Open Our Exist file and *fin is a object//
+    ofstream fout;                   //ofstream is a Function It'll Copy our Exist File Data to Another File and *fout is a object//
     char ch;
-    fin.open(f1);
-    fout.open(f2);
-    ch=fin.get();
-    while(!fin.eof())
+    fin.open(f1);                        //fin a function It'll entered in our Exist file 
+    fout.open(f2);                       //fout Is  a FUnction It'll open our new file
+    ch=fin.get();                          //get is function which read our exist file data
+    while(!fin.eof())                    //eof Is a Function It control the loop to the end of file data
     {
         fout<<ch;
         ch=fin.get();
     }
-    fin.close();
-    fout.close();
+    fin.close();                            //close is a function which close our exist file //
+    fout.close();                             //And also Close our New File//
 }
 int main()
 {
-    copyfile("chmmoo.txt","ABC.txt");
-    cout<<"File Copy is Completed ";
+    copyfile("chmmoo.txt","ABC.txt");                //"chmmoo.txt" Is a txt File Which Was already in our Computer
+    cout<<"File Copy is Completed ";                    //"ABC.txt" Is a new Created file and copy exist file data in this file
     getch();
 }
